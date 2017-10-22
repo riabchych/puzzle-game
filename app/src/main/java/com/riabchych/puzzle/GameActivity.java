a9c81ae7624041f93e5a56cf72eec4f6562c127e
Yevhenii Riabchych <769818+riabchych@users.noreply.github.com>
2017-10-22 20:53:14 +0300

    initial commit

---
+++ app/src/main/java/com/riabchych/puzzle/GameActivity.java
@@ -0,0 +1,13 @@
+package com.riabchych.puzzle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new DrawView(this));
+    }
+}
